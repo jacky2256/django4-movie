7b71353d166fab4e68a67e78cf4e6b25b24e0a50
jacky2256 <a.bojic22@gmail.com>
2023-06-11 16:05:02 +0300

    add multi languages en ru

--- django_movie/urls.py
+++ django_movie/urls.py
@@ -18,13 +18,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
+]
+
+urlpatterns += i18n_patterns(
     path('contact/', include("contact.urls")),
     path('', include('movie.urls')),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
-]
+)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
